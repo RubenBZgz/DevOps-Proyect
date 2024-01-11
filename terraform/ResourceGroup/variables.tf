53b23b93cf5e55c6019ac8ea59c115dfef0e90b5
rbaquedano <rbaquedano@kpmg.es>
2024-01-11 18:24:39 +0100

    azure terraform init

---
+++ terraform/ResourceGroup/variables.tf
@@ -0,0 +1,26 @@
+# Project variables
+
+variable "location" {
+  type        = string
+  description = "Deployment location"
+  default     = "West Europe"
+}
+
+variable "rsgname" {
+  type        = string
+  description = "Resource Group name"
+  default     = "TerraformRG"
+}
+
+variable "stgactname" {
+  type        = string
+  description = "Storage Account name"
+  default     = "storageaccountname"
+}
+
+variable "replication_type" {
+  type        = string
+  description = "Subscription replication type"
+  default     = "GRS"
+}
+#terraform plan -var="stgactname=prueba"
